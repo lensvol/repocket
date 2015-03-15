15836afb7b9f2bee9b5c13ec30da630a510eedb1
Kirill Borisov <lensvol@gmail.com>
2015-03-15 10:28:42 +0300

    Saving credentials into YAML config file.

--- repocket/main.py
+++ repocket/main.py
@@ -2,12 +2,39 @@
 
 import sys
 import click
+import os
+import yaml
 
 from pocket import Pocket
 
 
+def save_credentials(consumer_key, access_token, path=None):
+    if not path:
+        path = os.path.join(
+            os.path.expanduser('~'),
+            '.repocket.yml',
+        )
+
+    try:
+        with open(path, 'r') as fp:
+            cfg = yaml.load(fp.read())
+    except IOError:
+        cfg = {}
+
+    cfg['credentials'] = {
+        'consumer_key': consumer_key,
+        'access_token': access_token,
+    }
+
+    with open(path, 'w') as fp:
+        fp.write(yaml.dump(cfg))
+
+    return True
+
+
 def get_consumer_key():
     return click.prompt('Please enter your Pocket consumer key')
+
 
 def get_access_token(consumer_key):
     request_token = Pocket.get_request_token(
@@ -32,6 +59,8 @@ def get_access_token(consumer_key):
 if __name__ == '__main__':
     consumer_key = get_consumer_key()
     access_token = get_access_token(consumer_key)
+
+    save_credentials(consumer_key, access_token)
 
     click.echo('Your consumer key: {}'.format(consumer_key))
     click.echo('Your access token: {}'.format(access_token))
