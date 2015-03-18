ba3e16a4260263ea941d501ced8a3ebff050f0b7
Kirill Borisov <lensvol@gmail.com>
2015-03-18 10:59:07 +0300

    Fleshed out setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,40 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+description = 'Automatic rule-based tag suggestor for Pocket'
+if os.path.exists('README.md'):
+    with open('README.md', 'r') as fp:
+        long_description = fp.read()
+else:
+    long_description = description
+
+
 setup(
-    description='Automatic rule-based tag suggestor for Pocket',
+    description=description,
     name='repocket',
     zip_safe=False,
     author='Kirill Borisov',
     author_email='borisov.kir@gmail.com',
     url='https://github.com/lensvol/repocket',
+    keywords=['pocket', 'tag'],
+    long_description=long_description,
     version='0.0.1',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    license='MIT',
     install_requires=[
         'click==3.3',
         'pocket==0.3.5',
